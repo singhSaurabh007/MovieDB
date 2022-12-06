66d305d8c0bc6022536cf617684dc4623a0db2ae
Saurabh <ss1998singh@gmail.com>
2022-12-07 00:52:19 +0530

    add social icons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,12 @@ import React from "react"
 import logo from './logo.png';
 import "./Header.css"
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { 
+    faFacebook,
+    faInstagram,
+    faTwitter
+  } from "@fortawesome/free-brands-svg-icons";
 
 const Header = () => {
     return (
@@ -11,6 +17,14 @@ const Header = () => {
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+
+            <div className="headerRight">
+                <div class="social-container">
+                    <a href="https://www.facebook.com/harrypotter098/" className="facebook social"><FontAwesomeIcon icon={faFacebook} size="2x" /></a>
+                    <a href="https://www.instagram.com/harrry007/" className="instagram social"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
+                    <a href="https://twitter.com/___saurabh_____" className="twitter social"><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
+                </div>
             </div>
         </div>
     )

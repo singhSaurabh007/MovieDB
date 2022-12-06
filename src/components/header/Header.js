dc17b9a9932b2bd033237e5159880bb4d19cfb64
Saurabh <ss1998singh@gmail.com>
2022-12-06 22:31:18 +0530

    Updated logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import logo from './logo.png';
 import "./Header.css"
 import { Link } from "react-router-dom"
 
@@ -6,7 +7,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
+                <Link to="/"><img className="header__icon" src={logo} /></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
